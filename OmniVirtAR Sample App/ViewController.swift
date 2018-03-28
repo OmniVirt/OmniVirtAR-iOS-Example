bd09828cc054fe29cac13682b1a102bb094c9af6
Pairote Leelaphattarakij <sagarote@gmail.com>
2018-03-28 12:26:03 -0700

    Import source code for building the example app

---
+++ OmniVirtAR Sample App/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  OmniVirtAR Sample App
+//
+//  Copyright © 2018 OmniVirt. All rights reserved.
+//
+
+import UIKit
+import OmniVirtAR
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var button: LoadingButton!
+    
+    var adView: OVARVirtualObjectAdInterstitial!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        button.layer.cornerRadius = 25
+    }
+
+    @IBAction func onButtonTouchUpInside(_ sender: Any) {
+        button.showLoading()
+
+        adView = OVARVirtualObjectAdInterstitial()
+        adView.virtualObjectID = 26
+        adView.load(completion: {[weak self] () in
+            if let strongSelf = self {
+                strongSelf.button.hideLoading()
+                strongSelf.adView.present(fromRootViewController: strongSelf)
+            }
+        })
+    }
+
+}
+
+class LoadingButton: UIButton {
+    
+    var originalButtonText: String?
+    var activityIndicator: UIActivityIndicatorView!
+    
+    func showLoading() {
+        originalButtonText = self.titleLabel?.text
+        self.setTitle("", for: .normal)
+        
+        if (activityIndicator == nil) {
+            activityIndicator = createActivityIndicator()
+        }
+        
+        showSpinning()
+        isEnabled = false
+    }
+    
+    func hideLoading() {
+        self.setTitle(originalButtonText, for: .normal)
+        activityIndicator.stopAnimating()
+        isEnabled = true
+    }
+    
+    private func createActivityIndicator() -> UIActivityIndicatorView {
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.color = UIColor.white
+        activityIndicator.sizeToFit()
+        return activityIndicator
+    }
+    
+    private func showSpinning() {
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(activityIndicator)
+        centerActivityIndicatorInButton()
+        activityIndicator.startAnimating()
+    }
+    
+    private func centerActivityIndicatorInButton() {
+        let xCenterConstraint = NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal, toItem: activityIndicator, attribute: .centerX, multiplier: 1, constant: 0)
+        self.addConstraint(xCenterConstraint)
+        
+        let yCenterConstraint = NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal, toItem: activityIndicator, attribute: .centerY, multiplier: 1, constant: 0)
+        self.addConstraint(yCenterConstraint)
+    }
+    
+}

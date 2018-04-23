69250565a48ce56d0feebeda9e6b02ff6bab1d6f
Pairote Leelaphattarakij <sagarote@gmail.com>
2018-04-23 15:24:04 -0700

    Update example code for SDK v1.2.0

--- OmniVirtAR Sample App/ViewController.swift
+++ OmniVirtAR Sample App/ViewController.swift
@@ -10,7 +10,7 @@ import OmniVirtAR
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var button: LoadingButton!
+    @IBOutlet weak var button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,50 +24,3 @@ class ViewController: UIViewController {
     
 }
 
-class LoadingButton: UIButton {
-    
-    var originalButtonText: String?
-    var activityIndicator: UIActivityIndicatorView!
-    
-    func showLoading() {
-        originalButtonText = self.titleLabel?.text
-        self.setTitle("", for: .normal)
-        
-        if (activityIndicator == nil) {
-            activityIndicator = createActivityIndicator()
-        }
-        
-        showSpinning()
-        isEnabled = false
-    }
-    
-    func hideLoading() {
-        self.setTitle(originalButtonText, for: .normal)
-        activityIndicator.stopAnimating()
-        isEnabled = true
-    }
-    
-    private func createActivityIndicator() -> UIActivityIndicatorView {
-        let activityIndicator = UIActivityIndicatorView()
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.color = UIColor.white
-        activityIndicator.sizeToFit()
-        return activityIndicator
-    }
-    
-    private func showSpinning() {
-        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(activityIndicator)
-        centerActivityIndicatorInButton()
-        activityIndicator.startAnimating()
-    }
-    
-    private func centerActivityIndicatorInButton() {
-        let xCenterConstraint = NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal, toItem: activityIndicator, attribute: .centerX, multiplier: 1, constant: 0)
-        self.addConstraint(xCenterConstraint)
-        
-        let yCenterConstraint = NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal, toItem: activityIndicator, attribute: .centerY, multiplier: 1, constant: 0)
-        self.addConstraint(yCenterConstraint)
-    }
-    
-}
